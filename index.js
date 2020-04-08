31da6262c29079d17c2f3ba258fb22e49d63a264
ledung <lmdung94@gmail.com>
2020-04-08 10:44:22 +0700

    install pug

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const pug = require('pug');
 
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug')
+app.set('views', './views')
 app.get('/', (req, res) => {
-    res.send('i am dung dep trai')
+    res.render('index', {
+        name: 'Dung'
+    })
+})
+app.get('/users', (req, res) => {
+    res.render('./users/index', {
+        users: [
+            { id: 1, name: 'Chiu' },
+            { id: 2, name: 'Dung' }
+        ]
+    })
 });
+
 app.listen(port, () => {
     console.log('Listen on port:', port)
 })
