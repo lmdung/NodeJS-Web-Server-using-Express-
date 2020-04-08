4a4e68896ae4697b4b997ccfee9c43c9dd6994c2
ledung <lmdung94@gmail.com>
2020-04-08 16:13:06 +0700

    create view user

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const pug = require('pug');
+const low = require('lowdb')
+const shortid = require('shortid');
 
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 const app = express();
 const port = 3000;
 
+// Set some defaults
+db.defaults({ users: [] })
+  .write()
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
 app.set('view engine', 'pug')
 app.set('views', './views')
+
 app.get('/', (req, res) => {
     res.render('index', {
         name: 'Dung'
@@ -13,13 +27,35 @@ app.get('/', (req, res) => {
 })
 app.get('/users', (req, res) => {
     res.render('./users/index', {
-        users: [
-            { id: 1, name: 'Chiu' },
-            { id: 2, name: 'Dung' }
-        ]
+        users: db.get('users').value()
     })
 });
-
+app.get('/users/search', (req, res) => {
+    let q = req.query.q;
+    let users = db.get('users').value();
+    var matchedUsers = users.filter((user) => {
+        return user.name.toUpperCase().indexOf(q.toUpperCase()) !== -1;
+    })
+    res.render('./users/index', {
+        users: matchedUsers,
+        value: q,
+    })
+})
+app.get('/users/create', (req, res) => {
+    res.render('./users/create')
+})
+app.get('/users/:id', (req, res) => {
+    let id = req.params.id;
+    let user = db.get('users').find({id: id}).value()
+    res.render('./users/view', {
+        user: user
+    })
+})
+app.post('/users/create', (req, res) => {
+    req.body.id = shortid.generate();
+    db.get('users').push(req.body).write();
+    res.redirect('/users')
+})
 app.listen(port, () => {
     console.log('Listen on port:', port)
 })
